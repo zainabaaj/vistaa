1f1346203b8b0cf5b60cc075f46db9304419241b
Zeyna Baaj <zainabaaj@gmail.com>
2024-12-05 00:47:50 +0300

    color

--- vistaprojs/tailwind.config.js
+++ vistaprojs/tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+     navy: '#242b33'
       },
     },
   },
